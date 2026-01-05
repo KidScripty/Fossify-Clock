fc8fd8fd40e452829861dc5f7bb34a47e36c107c
AshBash <ash@tuxbet.com>
2026-01-05 19:14:34 +0000

    Digital Seconds widget with no abstraction
    Implementation based on duplicating the widgets and associated activities / helpers without refactoring shared functionality into abstracted classes.

---
+++ app/src/main/kotlin/org/fossify/clock/activities/WidgetDigitalSecondsConfigureActivity.kt
@@ -0,0 +1,181 @@
+package org.fossify.clock.activities
+
+import android.appwidget.AppWidgetManager
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.widget.SeekBar
+import org.fossify.clock.databinding.WidgetConfigDigitalSecondsBinding
+import org.fossify.clock.extensions.config
+import org.fossify.clock.helpers.FORMAT_12H_WITH_SECONDS
+import org.fossify.clock.helpers.FORMAT_24H_WITH_SECONDS
+import org.fossify.clock.helpers.MyDigitalTimeSecondsWidgetProvider
+import org.fossify.clock.helpers.SIMPLE_PHONE
+import org.fossify.commons.dialogs.ColorPickerDialog
+import org.fossify.commons.dialogs.FeatureLockedDialog
+import org.fossify.commons.extensions.adjustAlpha
+import org.fossify.commons.extensions.applyColorFilter
+import org.fossify.commons.extensions.getContrastColor
+import org.fossify.commons.extensions.getProperPrimaryColor
+import org.fossify.commons.extensions.isDynamicTheme
+import org.fossify.commons.extensions.isOrWasThankYouInstalled
+import org.fossify.commons.extensions.setFillWithStroke
+import org.fossify.commons.extensions.viewBinding
+import org.fossify.commons.helpers.IS_CUSTOMIZING_COLORS
+
+class WidgetDigitalSecondsConfigureActivity : SimpleActivity() {
+    private var mBgAlpha = 0f
+    private var mWidgetId = 0
+    private var mBgColor = 0
+    private var mTextColor = 0
+    private var mBgColorWithoutTransparency = 0
+    private var mFeatureLockedDialog: FeatureLockedDialog? = null
+    private val binding: WidgetConfigDigitalSecondsBinding by viewBinding(WidgetConfigDigitalSecondsBinding::inflate)
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        useDynamicTheme = false
+        super.onCreate(savedInstanceState)
+        setResult(RESULT_CANCELED)
+        setContentView(binding.root)
+        setupEdgeToEdge(padTopSystem = listOf(binding.configDigitalSecondsHolder), padBottomSystem = listOf(binding.root))
+        initVariables()
+
+        mWidgetId = intent.extras?.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID)
+            ?: AppWidgetManager.INVALID_APPWIDGET_ID
+
+        if (!config.wasInitialWidgetSetUp && Build.BRAND.equals(SIMPLE_PHONE, true)) {
+            saveConfig()
+            config.wasInitialWidgetSetUp = true
+            return
+        }
+
+        val isCustomizingColors = intent.extras?.getBoolean(IS_CUSTOMIZING_COLORS) == true
+        if (mWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID && !isCustomizingColors) {
+            finish()
+        }
+
+        binding.configDigitalSecondsSave.setOnClickListener { saveConfig() }
+        binding.configDigitalSecondsBgColor.setOnClickListener { pickBackgroundColor() }
+        binding.configDigitalSecondsTextColor.setOnClickListener { pickTextColor() }
+
+        val primaryColor = getProperPrimaryColor()
+        binding.configDigitalSecondsBgSeekbar.setColors(mTextColor, primaryColor, primaryColor)
+
+        if (!isCustomizingColors && !isOrWasThankYouInstalled()) {
+            mFeatureLockedDialog = FeatureLockedDialog(this) {
+                if (!isOrWasThankYouInstalled()) {
+                    finish()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (mFeatureLockedDialog != null && isOrWasThankYouInstalled()) {
+            mFeatureLockedDialog?.dismissDialog()
+        }
+    }
+
+    private fun initVariables() {
+        mBgColor = config.widgetBgColor
+        mBgAlpha = Color.alpha(mBgColor) / 255.toFloat()
+        mBgColorWithoutTransparency = Color.rgb(
+            Color.red(mBgColor), Color.green(mBgColor), Color.blue(mBgColor)
+        )
+
+        binding.configDigitalSecondsBgSeekbar.setOnSeekBarChangeListener(bgSeekbarChangeListener)
+        binding.configDigitalSecondsBgSeekbar.progress = (mBgAlpha * 100).toInt()
+        updateBackgroundColor()
+
+        mTextColor = config.widgetTextColor
+        if (
+            mTextColor == resources.getColor(org.fossify.commons.R.color.default_widget_text_color)
+            && isDynamicTheme()
+        ) {
+            mTextColor = resources.getColor(org.fossify.commons.R.color.you_primary_color, theme)
+        }
+
+        updateTextColor()
+
+        val clockFormat = if (config.use24HourFormat) FORMAT_24H_WITH_SECONDS else FORMAT_12H_WITH_SECONDS
+        binding.configDigitalSecondsTime.format24Hour = clockFormat
+        binding.configDigitalSecondsTime.format12Hour = clockFormat
+    }
+
+    private fun saveConfig() {
+        storeWidgetColors()
+        requestWidgetUpdate()
+
+        Intent().apply {
+            putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mWidgetId)
+            setResult(RESULT_OK, this)
+        }
+        finish()
+    }
+
+    private fun storeWidgetColors() {
+        config.apply {
+            widgetBgColor = mBgColor
+            widgetTextColor = mTextColor
+        }
+    }
+
+    private fun pickBackgroundColor() {
+        ColorPickerDialog(this, mBgColorWithoutTransparency) { wasPositivePressed, color ->
+            if (wasPositivePressed) {
+                mBgColorWithoutTransparency = color
+                updateBackgroundColor()
+            }
+        }
+    }
+
+    private fun pickTextColor() {
+        ColorPickerDialog(this, mTextColor) { wasPositivePressed, color ->
+            if (wasPositivePressed) {
+                mTextColor = color
+                updateTextColor()
+            }
+        }
+    }
+
+    private fun requestWidgetUpdate() {
+        Intent(
+            AppWidgetManager.ACTION_APPWIDGET_UPDATE,
+            null,
+            this,
+            MyDigitalTimeSecondsWidgetProvider::class.java
+        ).apply {
+            putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, intArrayOf(mWidgetId))
+            sendBroadcast(this)
+        }
+    }
+
+    private fun updateTextColor() {
+        binding.configDigitalSecondsTextColor.setFillWithStroke(mTextColor, mTextColor)
+        binding.configDigitalSecondsTime.setTextColor(mTextColor)
+        binding.configDigitalSecondsDate.setTextColor(mTextColor)
+        binding.configDigitalSecondsSave.setTextColor(getProperPrimaryColor().getContrastColor())
+    }
+
+    private fun updateBackgroundColor() {
+        mBgColor = mBgColorWithoutTransparency.adjustAlpha(mBgAlpha)
+        binding.configDigitalSecondsBgColor.setFillWithStroke(mBgColor, mBgColor)
+        binding.configDigitalSecondsBackground.applyColorFilter(mBgColor)
+        binding.configDigitalSecondsSave.backgroundTintList =
+            ColorStateList.valueOf(getProperPrimaryColor())
+    }
+
+    private val bgSeekbarChangeListener = object : SeekBar.OnSeekBarChangeListener {
+        override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+            mBgAlpha = progress.toFloat() / 100.toFloat()
+            updateBackgroundColor()
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+        override fun onStopTrackingTouch(seekBar: SeekBar) {}
+    }
+}
